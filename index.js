0d2afd1dd831b460ffa349b0e59acb2a6486924f
imaryannn <itzarynnn@gmail.com>
2026-04-12 21:06:15 +0530

    Fix serverless function syntax

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = (req, res) => {
-  res.setHeader('Content-Type', 'text/html');
   const fs = require('fs');
   const path = require('path');
   const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
-  res.send(html);
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.status(200).end(html);
 };
