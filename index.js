0fdfedb7c2fa09b271119154d39c1930f3766d84
imaryannn <itzarynnn@gmail.com>
2026-04-12 21:09:50 +0530

    Fix index.js path resolution

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = (req, res) => {
-  const fs = require('fs');
-  const path = require('path');
-  const html = fs.readFileSync(path.join(__dirname, 'index.html'), 'utf8');
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  const html = fs.readFileSync(path.resolve('index.html'), 'utf8');
+  res.setHeader('Content-Type', 'text/html');
   res.status(200).end(html);
 };
